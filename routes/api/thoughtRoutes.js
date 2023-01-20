8fe7371600d28db47da8e7801284010aec4f6a9e
dratsavong <dratsavong@ymail.com>
2023-01-20 01:27:29 -0500

    added thought and user routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    removeThought,
+    createReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController.js');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+    .delete(removeReaction);
+
+module.exports = router;
